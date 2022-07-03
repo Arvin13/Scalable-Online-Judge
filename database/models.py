ab7207c53db4a39bbbb849afcfb76834ed223b94
Arvind Meena <arvindmeena9414@gmail.com>
2022-07-03 18:49:07 +0530

    Add 'Contest' Model to database

--- database/models.py
+++ database/models.py
@@ -5,18 +5,31 @@ class Users(models.Model):
     User_ID = models.ForeignKey(User, on_delete=models.CASCADE)
     total_score = models.FloatField(default = 0)
 
+class Contest(models.Model):
+        con_id = "Contest 1"
+        Contest_ID = models.CharField(max_length=100, default = con_id)
+        Contest_name = models.CharField(max_length=100, default = con_id)
+        def __str__(self):
+            return self.Contest_name
+
 class Problem(models.Model):
+    contest = models.ForeignKey(Contest, on_delete = models.CASCADE)
     Problem_ID = models.CharField(max_length=100)
     Problem_name = models.CharField(max_length = 100)
     description = models.CharField(max_length = 200)
     difficulty = models.CharField(max_length = 50)
     solved_status = models.CharField(max_length = 10, default = 'Unsolved')
     Score = models.FloatField()
+    def __str__(self):
+        return self.Problem_name
 
 class test_case(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
     Input = models.CharField(max_length = 100)
     Output = models.CharField(max_length = 100)
+    def __str__(self):
+        io = "Input:  %s    "%self.Input + "||    Outputi:  %s"%self.Output
+        return io
 
 class Submission(models.Model):
     Userp = models.ForeignKey(Users, on_delete = models.CASCADE)
